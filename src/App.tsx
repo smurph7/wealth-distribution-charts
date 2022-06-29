6b979b6f3a2b01e5334ebb97e44fda4c1088fe7d
Sarah <sarahnmurphy93@gmail.com>
2022-06-30 00:01:47 +0100

    Refactor style of pie chart

--- src/App.tsx
+++ src/App.tsx
@@ -5,9 +5,13 @@ import { WealthDistributionPieChart } from './wealth-distribution-pie-chart';
 
 function App() {
   return (
-    <Flex direction="column" align="center">
-      <Heading pt={4}>Wealth Distribution in Ireland</Heading>
-      <WealthDistributionPieChart />
+    <Flex direction="column" align="center" gap={8} p={4}>
+      <Heading pt={4} textAlign="center">
+        Wealth Distribution in Ireland (2020)
+      </Heading>
+      <Flex direction="column" align="center">
+        <WealthDistributionPieChart />
+      </Flex>
     </Flex>
   );
 }
--- src/wealth-distribution-pie-chart.tsx
+++ src/wealth-distribution-pie-chart.tsx
@@ -1,3 +1,4 @@
+import * as React from 'react';
 import {
   Pie,
   PieChart,
@@ -7,24 +8,82 @@ import {
   Tooltip
 } from 'recharts';
 import type { PieLabelRenderProps, TooltipProps } from 'recharts';
-import { Box, Text, useMediaQuery } from '@chakra-ui/react';
+import {
+  Box,
+  Flex,
+  Text,
+  useMediaQuery,
+  theme,
+  Link,
+  Stack
+} from '@chakra-ui/react';
 
 const data = [
-  { name: '50%', value: 1.4 },
-  { name: '30%', value: 25.9 },
-  { name: '20%', value: 72.7 }
+  { name: '< 20th', value: 10.8 },
+  { name: '20-39th', value: 13.0 },
+  { name: '40-59th', value: 15.1 },
+  { name: '60-79th', value: 19.3 },
+  { name: 'Top 20th', value: 42.0 }
 ];
 
-const COLORS = ['#00C49F', '#FFBB28', '#FF8042'];
+const COLORS = [
+  theme.colors.blue[500],
+  theme.colors.green[500],
+  theme.colors.yellow[500],
+  theme.colors.orange[500],
+  theme.colors.red[500]
+];
+
+const RADIAN = Math.PI / 180;
+const CustomisedLabel = ({
+  cx,
+  cy,
+  midAngle,
+  innerRadius,
+  outerRadius,
+  percent,
+  name
+}: any) => {
+  const [isMobile] = useMediaQuery('(max-width: 768px)');
+
+  const radius = innerRadius + (outerRadius - innerRadius) * 0.5;
+  const x = cx + radius * Math.cos(-midAngle * RADIAN);
+  const y = cy + radius * Math.sin(-midAngle * RADIAN);
+
+  return (
+    <g>
+      <text
+        x={x - 5}
+        y={y - 10}
+        fill="white"
+        fontSize={isMobile ? 12 : 20}
+        textAnchor={x > cx ? 'start' : 'end'}
+        dominantBaseline="central"
+      >
+        {`${name}`}
+      </text>
+      <text
+        x={x}
+        y={y + 10}
+        fill="white"
+        fontSize={isMobile ? 12 : 20}
+        textAnchor={x > cx ? 'start' : 'end'}
+        dominantBaseline="central"
+      >
+        {`${(percent * 100).toFixed(0)}%`}
+      </text>
+    </g>
+  );
+};
 
 const valueAccessor =
   (attribute: string | number) =>
   ({ payload }: { payload: PieLabelRenderProps }): string => {
     const [isMobile] = useMediaQuery('(max-width: 768px)');
+
     return isMobile
-      ? `${payload[attribute]}%`
-      : `${payload[attribute]}% of wealth is owned by ${payload['name']} of
-      the population`;
+      ? ''
+      : `${payload['name']} percentile has ${payload[attribute]}% of wealth`;
   };
 
 const CustomTooltip = ({
@@ -32,14 +91,14 @@ const CustomTooltip = ({
   payload
 }: TooltipProps<'value', 'name'>): JSX.Element => {
   const [isMobile] = useMediaQuery('(max-width: 768px)');
-  console.log('isMobile', isMobile);
+
   if (!isMobile) return <></>;
+
   if (active && payload && payload.length) {
     return (
-      <Box width={200}>
+      <Box width={200} bg="rgba(255,255,255, 0.6)" borderRadius={4}>
         <Text fontWeight="bold">
-          {payload[0].value}% of wealth is owned by {payload[0].name}of the
-          population
+          {payload[0].name} percentile has {payload[0].value}% of wealth
         </Text>
       </Box>
     );
@@ -49,17 +108,46 @@ const CustomTooltip = ({
 };
 
 export const WealthDistributionPieChart = (): JSX.Element => {
+  //   const [isAnimationActive, setIsAnimationActive] = React.useState(false);
+  //   const { data: wealthDistributionData, ...query } =
+  //     useWealthDistributionByPercentile();
+  //   console.log('query', wealthDistributionData);
+  //   if (query.isSuccess) {
+  //     console.log(
+  //       Object.keys(wealthDistributionData).map(item =>
+  //         console.log(
+  //           'item',
+  //           Object.values(wealthDistributionData[item].category.label)
+  //         )
+  //       )
+  //     );
+  //   }
+
+  //   React.useEffect(() => {
+  //     if (query.isSuccess) {
+  //       setIsAnimationActive(true);
+  //     }
+  //   }, [query]);
+
   return (
-    <Box width={[400, 400, 600, 800]} height={[400, 400, 600, 600]}>
+    <Flex
+      direction="column"
+      width={[380, 380, 600, 800]}
+      height={[380, 380, 600, 600]}
+      overflowX="hidden"
+    >
       <ResponsiveContainer width="100%" height="100%">
         <PieChart width={800} height={600}>
           <Pie
             data={data}
+            // isAnimationActive={isAnimationActive}
             dataKey="value"
             nameKey="name"
             cx="50%"
             cy="50%"
-            outerRadius="50%"
+            outerRadius="90%"
+            label={<CustomisedLabel />}
+            labelLine={false}
           >
             <LabelList
               position="outside"
@@ -80,9 +168,18 @@ export const WealthDistributionPieChart = (): JSX.Element => {
               />
             ))}
           </Pie>
-          <Tooltip content={<CustomTooltip />} />
+          <Tooltip
+            content={<CustomTooltip />}
+            allowEscapeViewBox={{ x: true, y: false }}
+          />
         </PieChart>
       </ResponsiveContainer>
-    </Box>
+      <Stack align="flex-end" p={4}>
+        <Text>By Percentile of Household Income 2020</Text>
+        <Link href="https://data.cso.ie/table/HFC2038" color="blue.500">
+          Data provided by CSO
+        </Link>
+      </Stack>
+    </Flex>
   );
 };
